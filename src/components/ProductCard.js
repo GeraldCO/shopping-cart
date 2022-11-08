bfce9e3f57b810f9c276ba050bd255ccd5dea1ac
Gerald <geraldcerd@gmail.com>
2022-11-08 17:27:44 -0600

    office changes

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from 'react';
+import classes from './ProductCard.module.css';
+
 
 export default function ProductCard(props){
     const {name, gallery, price} = props.product;
     return(
-        <div className='product-card-container'>
+        <div className='container product-card-container'>
 
             <div className='product-picture-container'>
-                <img src={gallery[0]} alt="shop product" />
-            </div>            
+                <img src={gallery[0]} alt={name} />
+            </div>
+            <div className='d-flex'>
+                <span className="btn btn-dark">
+                    Black Friday
+                </span>
+                <span className="btn btn-outline-success">
+                    Free Shipping 
+                </span>
+            </div>
             <div>
                 <h4>{name}</h4>
             </div>
